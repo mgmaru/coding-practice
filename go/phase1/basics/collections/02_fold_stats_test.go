933739fa71b0210effe6052524859d71235a1b92
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-06-15 17:47:52 +0900

    fix: レビューの指摘をすべて修正, 残件すべて完了

--- go/phase1/basics/collections/02_fold_stats_test.go
+++ go/phase1/basics/collections/02_fold_stats_test.go
@@ -7,19 +7,28 @@ import (
 
 func TestCalcSumAndMaxAndMin(t *testing.T) {
 
-	var buf bytes.Buffer
+	// バグ：ここでbufを定義してしまうと、テストケースを検証するごとにbufに文字列が書き込まれてしまう。
+	// var buf bytes.Buffer
 
 	cases := []struct {
 		Name  string
 		Input []int
 		Want  string
 	}{
-		{"normal", []int{3, 1, 4, 1, 5, 9, 2, 6}, "合計=31, 最大=9, 最小=1"},
+		{"normal_1", []int{3, 1, 4, 1, 5, 9, 2, 6}, "合計=31, 最大=9, 最小=1"},
+		// 修正：スライス要素が負の場合
+		{"normal_2", []int{-3, -1, -5}, "合計=-9, 最大=-1, 最小=-5"},
+		// 修正：要素が１つの場合
+		{"normal_3", []int{42}, "合計=42, 最大=42, 最小=42"},
 		{"nilSlice", nil, "入力されたスライスがnilです。"},
+		{"emptySlice", []int{}, "入力されたスライスが空です。"},
 	}
 
 	for _, c := range cases {
 		t.Run(c.Name, func(t *testing.T) {
+
+			//修正：bufはテストケースごとに初期化（バグ：初期化しないと次々と書き込まれていく）
+			var buf bytes.Buffer
 
 			calcSumAndMaxAndMin(&buf, c.Input) // bufで文字列のbyteを受け取る。
 			gotValue := buf.String()           // bufを文字列に変換
