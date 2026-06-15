2741079f3d713c7c0c8c34880ad9d5f291852616
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-06-15 17:00:01 +0900

    初回実装

--- go/phase1/basics/collections/02_fold_stats.go
+++ go/phase1/basics/collections/02_fold_stats.go
@@ -1,1 +1,44 @@
 package collections
+
+import (
+	"fmt"
+	"io"
+)
+
+// スライス要素の合計と最大値、最小値を計算して表示する関数
+// 文字列のbyteを格納する変数w io.Writerも渡してあげる。
+func calcSumAndMaxAndMin(w io.Writer, input []int) {
+
+	var sum int
+	var max int
+	var min int
+
+	// 入力inputがnilの場合は、何も返さない。
+	if input == nil {
+		fmt.Fprintf(w, "入力されたスライスがnilです。")
+		return
+	}
+
+	for i, num := range input {
+
+		// 要素1個目は初期値を代入
+		if i == 0 {
+			max = num
+			min = num
+		}
+
+		// 要素2個目以降
+
+		sum += num
+
+		// 要素が最大値を超えた場合、最大値を入れ替え
+		if max < num {
+			max = num
+		}
+		// 要素が最小値よりも小さい場合、最小値を入れ替え
+		if min > num {
+			min = num
+		}
+	}
+	fmt.Fprintf(w, "合計=%d, 最大=%d, 最小=%d", sum, max, min)
+}
--- go/phase1/basics/collections/02_fold_stats_test.go
+++ go/phase1/basics/collections/02_fold_stats_test.go
@@ -1,1 +1,34 @@
 package collections
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCalcSumAndMaxAndMin(t *testing.T) {
+
+	var buf bytes.Buffer
+
+	cases := []struct {
+		Name  string
+		Input []int
+		Want  string
+	}{
+		{"normal", []int{3, 1, 4, 1, 5, 9, 2, 6}, "合計=31, 最大=9, 最小=1"},
+		{"nilSlice", nil, "入力されたスライスがnilです。"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+
+			calcSumAndMaxAndMin(&buf, c.Input) // bufで文字列のbyteを受け取る。
+			gotValue := buf.String()           // bufを文字列に変換
+
+			// 比較(文字列を比較)
+			if gotValue != c.Want {
+				t.Errorf("期待する結果が違います。%s, %s\n", gotValue, c.Want)
+			}
+
+		})
+	}
+}
