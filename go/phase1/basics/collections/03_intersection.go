e8d27eb1e1df826b8c42e32888728e8a077a8ce5
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-06-16 20:14:45 +0900

    初回実装

--- go/phase1/basics/collections/03_intersection.go
+++ go/phase1/basics/collections/03_intersection.go
@@ -1,1 +1,52 @@
 package collections
+
+// 課題：Aの要素がBに含まれるかどうかを判定し、含まれるものを返す。
+
+// 線形探索
+// 設計意思：課題では、[]int型の例を示しているが、スライスの中にstringなどの他の型が入ってきても良いように、any型にした。
+// 設計意思：今回は、あくまで線形探索およびsetによる探索の比較であるので、[]any型とした。しかし、本来の実装においては、any型は使用しない方が良いと考える。
+
+func linearSearchElements(inputA []any, inputB []any) []any {
+
+	// 一致する要素を格納する配列
+	containingElements := make([]any, 0)
+	// 設計意思：入力がnilもしくは空スライスの場合のガードはしない。
+	// 根拠：make([]any, 0)でスライスを初期化しているので、入力にnilもしくは空スライスが入ってきた時に返されるのは、空スライスが返る。これは想定した挙動である。
+	// また、入力Aの要素がBに含まれていなかった場合にも、空のスライスが返る。これも想定した挙動。
+
+	for _, a := range inputA {
+		for _, b := range inputB {
+			if a == b {
+				containingElements = append(containingElements, b) //ここ、aで書くべきかbで書くべきか...今回は、bのスライスでループを回しているのでbにした。
+			}
+		}
+	}
+	// 計算量：n個の要素に対してn個の操作　-> O(n^2)
+
+	return containingElements
+}
+
+// set探索
+func setSearchElements(inputA []any, inputB []any) []any {
+
+	// 一致する要素を格納する配列
+	containingElements := make([]any, 0)
+
+	// キーにはany、値には空のstructが入るmapを定義
+	set := make(map[any]struct{})
+
+	// setのキーにAの要素、値に空のstructを格納（Aの要素の集合を格納）
+	for _, a := range inputA {
+		set[a] = struct{}{}
+	}
+
+	// inputBの要素１つ１つに対して、setに含まれるか判定
+	for _, b := range inputB {
+		if _, ok := set[b]; ok {
+			containingElements = append(containingElements, b)
+		}
+	}
+	// 計算量：n個の要素に対してn+n(=2n)回の操作　-> O(2n) -> O(n) （定数倍無視）
+
+	return containingElements
+}
--- go/phase1/basics/collections/03_intersection_test.go
+++ go/phase1/basics/collections/03_intersection_test.go
@@ -1,1 +1,49 @@
 package collections
+
+import (
+	"reflect"
+	"testing"
+)
+
+// 設計意思：グローバル変数でテストケースを定義（2つの関数で使いまわすため）
+type Case struct {
+	Name   string
+	inputA []any
+	inputB []any
+	Want   []any
+}
+
+var cases = []Case{
+	{"IntSliceAndAisLonger", []any{1, 2, 3, 4, 5}, []any{2, 4, 6}, []any{2, 4}},
+	{"IntSliceAndBisLonger", []any{2, 4, 6}, []any{1, 2, 3, 4, 5}, []any{2, 4}},
+	{"StringSliceAndAisLonger", []any{"apple", "banana", "grape", "lemon"}, []any{"grape"}, []any{"grape"}},
+	{"StringSlicendBisLonger", []any{"grape"}, []any{"apple", "banana", "grape", "lemon"}, []any{"grape"}},
+	{"AnySlice", []any{1, "apple", 2, "lemon"}, []any{2, "lemon", 4, "banana"}, []any{2, "lemon"}},
+	{"AisEmptySlice", []any{}, []any{2, 4, 6}, []any{}},
+	{"BisEmptySlice", []any{1, 2, 3, 4, 5}, []any{}, []any{}},
+	{"AisNilSlice", nil, []any{2, 4, 6}, []any{}},
+	{"BisNilSlice", []any{1, 2, 3, 4, 5}, nil, []any{}},
+	{"notIcluded", []any{1, 2, 3, 4, 5}, []any{6, 7, 8}, []any{}},
+}
+
+func TestLinearSearchElements(t *testing.T) {
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			gotValue := linearSearchElements(c.inputA, c.inputB)
+			if !reflect.DeepEqual(gotValue, c.Want) {
+				t.Errorf("期待する値が違います。 gotValue=%v , Want=%v", gotValue, c.Want)
+			}
+		})
+	}
+}
+
+func TestSetSearchElements(t *testing.T) {
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			gotValue := linearSearchElements(c.inputA, c.inputB)
+			if !reflect.DeepEqual(gotValue, c.Want) {
+				t.Errorf("期待する値が違います。 gotValue=%v , Want=%v", gotValue, c.Want)
+			}
+		})
+	}
+}
