8514b215ed13a369cada3fcbaea6dc0b6c2197a3
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-06-28 18:05:56 +0900

    fix: レビューの指摘部分を修正

--- go/phase1/basics/collections/05_group_by.go
+++ go/phase1/basics/collections/05_group_by.go
@@ -5,42 +5,49 @@ type person struct {
 	dept string // 部門
 }
 
-// keyを受け取ってkeyごとのリストを返す関数
-func groupBy(key string, input []person) map[string][]string {
+// keyOf/valueOfでpersonからキー・値を取り出し、キーごとに値をまとめて返す関数
+func groupBy(keyOf func(person) string, valueOf func(person) string, input []person) map[string][]string {
+	// 修正：keyOf：person型の変数を受け取って、keyとなる要素を返す関数
+	// 修正：valueOf：person型の変数を受け取って、valueとなる要素を返す関数
+	// 理由：関数をgroupByの引数として渡してあげることで、呼び出し元でkeyとvalueの要素を注入する。
 
 	m := make(map[string][]string, len(input))
 
 	// keyが正しく指定できていない
-	if key != "name" && key != "dept" {
-		return m // 空
-	}
+	// 修正：keyOf関数をgroupByの引数で渡すことで、不正なキーを検証する意味がなくなった。
+	// if key != "name" && key != "dept" {
+	// 	return m // 空
+	// }
 
 	// inputが空もしくはnil
-	if len(input) == 0 {
-		return m // 空
-	}
+	// 修正：len(input) == 0 を消去
+	// 理由：inputが空もしくはnilの時は、以下のfor文が1回も回らず、いずれの場合も空のmが返るので不要。
 
-	// 以下、keyが正しく指定されている
-	for _, person := range input {
-		m[person.keyFunc(key)] = append(m[person.keyFunc(key)], person.valueFunc(key))
+	// 修正：forの中の変数「person」を「p」へ修正。
+	// 理由：personは型と命名が被っているので、forの中でperson型を使おうとしても使えなくなる。
+	for _, p := range input {
+		k := keyOf(p) // 修正：関数呼び出しを1回にまとめた。
+		m[k] = append(m[k], valueOf(p))
 	}
 	return m
 }
 
 // キーと同じ名前のフィールド要素を返す関数
-func (p person) keyFunc(key string) string {
-	if key == "name" { // name
-		return p.name
-	} else { // dept
-		return p.dept
-	}
-}
+// 修正：不要になったので削除。
+// func (p person) keyFunc(key string) string {
+// 	if key == "name" { // name
+// 		return p.name
+// 	} else { // dept
+// 		return p.dept
+// 	}
+// }
 
 // keyに対応する要素を返す関数
-func (p person) valueFunc(key string) string {
-	if key == "name" { // keyがnameだったらdeptを返す
-		return p.dept
-	} else { // keyがdeptだったらnameを返す
-		return p.name
-	}
-}
+// 修正：不要になったので削除
+// func (p person) valueFunc(key string) string {
+// 	if key == "name" { // keyがnameだったらdeptを返す
+// 		return p.dept
+// 	} else { // keyがdeptだったらnameを返す
+// 		return p.name
+// 	}
+// }
--- go/phase1/basics/collections/05_group_by_test.go
+++ go/phase1/basics/collections/05_group_by_test.go
@@ -8,22 +8,26 @@ import (
 func TestGroupBy(t *testing.T) {
 	cases := []struct {
 		name         string
-		inputKey     string
+		keyOf        func(person) string
+		valueOf      func(person) string
 		inputPersons []person
 		want         map[string][]string
 	}{
-		{name: "normal", inputKey: "dept", inputPersons: []person{{name: "Sato", dept: "Sales"}, {name: "Suzuki", dept: "Dev"}, {name: "Tanaka", dept: "Sales"}}, want: map[string][]string{"Sales": {"Sato", "Tanaka"}, "Dev": {"Suzuki"}}},
-		{name: "invalidKey", inputKey: "age", inputPersons: []person{{name: "Sato", dept: "Sales"}, {name: "Suzuki", dept: "Dev"}, {name: "Tanaka", dept: "Sales"}}, want: map[string][]string{}},
-		{name: "duplicateName", inputKey: "name", inputPersons: []person{{name: "Sato", dept: "Dev"}, {name: "Saito", dept: "Sales"}, {name: "Sato", dept: "Sales"}}, want: map[string][]string{"Sato": {"Dev", "Sales"}, "Saito": {"Sales"}}},
-		{name: "duplicateNameAndDept", inputKey: "dept", inputPersons: []person{{name: "Sato", dept: "Dev"}, {name: "Tanaka", dept: "Sales"}, {name: "Sato", dept: "Dev"}}, want: map[string][]string{"Dev": {"Sato", "Sato"}, "Sales": {"Tanaka"}}},
-		{name: "EmptyInput", inputKey: "dept", inputPersons: []person{}, want: map[string][]string{}},
-		{name: "NilInput", inputKey: "dept", inputPersons: nil, want: map[string][]string{}},
+		// 修正： keyOfとvalueOfに関数を渡す。
+		{name: "normal", keyOf: func(p person) string { return p.dept }, valueOf: func(p person) string { return p.name }, inputPersons: []person{{name: "Sato", dept: "Sales"}, {name: "Suzuki", dept: "Dev"}, {name: "Tanaka", dept: "Sales"}}, want: map[string][]string{"Sales": {"Sato", "Tanaka"}, "Dev": {"Suzuki"}}},
+		// {name: "invalidKey", inputKey: "age", inputPersons: []person{{name: "Sato", dept: "Sales"}, {name: "Suzuki", dept: "Dev"}, {name: "Tanaka", dept: "Sales"}}, want: map[string][]string{}},
+		{name: "duplicateName", keyOf: func(p person) string { return p.name }, valueOf: func(p person) string { return p.dept }, inputPersons: []person{{name: "Sato", dept: "Dev"}, {name: "Saito", dept: "Sales"}, {name: "Sato", dept: "Sales"}}, want: map[string][]string{"Sato": {"Dev", "Sales"}, "Saito": {"Sales"}}},
+		{name: "duplicateNameAndDept", keyOf: func(p person) string { return p.dept }, valueOf: func(p person) string { return p.name }, inputPersons: []person{{name: "Sato", dept: "Dev"}, {name: "Tanaka", dept: "Sales"}, {name: "Sato", dept: "Dev"}}, want: map[string][]string{"Dev": {"Sato", "Sato"}, "Sales": {"Tanaka"}}},
+		{name: "EmptyInput", keyOf: func(p person) string { return p.dept }, valueOf: func(p person) string { return p.name }, inputPersons: []person{}, want: map[string][]string{}},
+		{name: "NilInput", keyOf: func(p person) string { return p.dept }, valueOf: func(p person) string { return p.name }, inputPersons: nil, want: map[string][]string{}},
+		// 修正：重複がないテストケースを追加
+		{name: "noDuplicate", keyOf: func(p person) string { return p.dept }, valueOf: func(p person) string { return p.name }, inputPersons: []person{{name: "Sato", dept: "Dev"}, {name: "Tanaka", dept: "Account"}, {name: "Suzuki", dept: "Sales"}}, want: map[string][]string{"Dev": {"Sato"}, "Account": {"Tanaka"}, "Sales": {"Suzuki"}}},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
 
-			gotValue := groupBy(c.inputKey, c.inputPersons)
+			gotValue := groupBy(c.keyOf, c.valueOf, c.inputPersons)
 
 			if !reflect.DeepEqual(gotValue, c.want) {
 				t.Errorf("期待する値と違います。gotValue= %v, want= %v", gotValue, c.want)
