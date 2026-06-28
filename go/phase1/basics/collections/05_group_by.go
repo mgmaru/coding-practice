872aea8054d4b06118158b783247c9679910756b
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-06-28 16:44:19 +0900

    初回実装

--- go/phase1/basics/collections/05_group_by.go
+++ go/phase1/basics/collections/05_group_by.go
@@ -1,1 +1,46 @@
 package collections
+
+type person struct {
+	name string // 名前
+	dept string // 部門
+}
+
+// keyを受け取ってkeyごとのリストを返す関数
+func groupBy(key string, input []person) map[string][]string {
+
+	m := make(map[string][]string, len(input))
+
+	// keyが正しく指定できていない
+	if key != "name" && key != "dept" {
+		return m // 空
+	}
+
+	// inputが空もしくはnil
+	if len(input) == 0 {
+		return m // 空
+	}
+
+	// 以下、keyが正しく指定されている
+	for _, person := range input {
+		m[person.keyFunc(key)] = append(m[person.keyFunc(key)], person.valueFunc(key))
+	}
+	return m
+}
+
+// キーと同じ名前のフィールド要素を返す関数
+func (p person) keyFunc(key string) string {
+	if key == "name" { // name
+		return p.name
+	} else { // dept
+		return p.dept
+	}
+}
+
+// keyに対応する要素を返す関数
+func (p person) valueFunc(key string) string {
+	if key == "name" { // keyがnameだったらdeptを返す
+		return p.dept
+	} else { // keyがdeptだったらnameを返す
+		return p.name
+	}
+}
--- go/phase1/basics/collections/05_group_by_test.go
+++ go/phase1/basics/collections/05_group_by_test.go
@@ -1,1 +1,34 @@
 package collections
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGroupBy(t *testing.T) {
+	cases := []struct {
+		name         string
+		inputKey     string
+		inputPersons []person
+		want         map[string][]string
+	}{
+		{name: "normal", inputKey: "dept", inputPersons: []person{{name: "Sato", dept: "Sales"}, {name: "Suzuki", dept: "Dev"}, {name: "Tanaka", dept: "Sales"}}, want: map[string][]string{"Sales": {"Sato", "Tanaka"}, "Dev": {"Suzuki"}}},
+		{name: "invalidKey", inputKey: "age", inputPersons: []person{{name: "Sato", dept: "Sales"}, {name: "Suzuki", dept: "Dev"}, {name: "Tanaka", dept: "Sales"}}, want: map[string][]string{}},
+		{name: "duplicateName", inputKey: "name", inputPersons: []person{{name: "Sato", dept: "Dev"}, {name: "Saito", dept: "Sales"}, {name: "Sato", dept: "Sales"}}, want: map[string][]string{"Sato": {"Dev", "Sales"}, "Saito": {"Sales"}}},
+		{name: "duplicateNameAndDept", inputKey: "dept", inputPersons: []person{{name: "Sato", dept: "Dev"}, {name: "Tanaka", dept: "Sales"}, {name: "Sato", dept: "Dev"}}, want: map[string][]string{"Dev": {"Sato", "Sato"}, "Sales": {"Tanaka"}}},
+		{name: "EmptyInput", inputKey: "dept", inputPersons: []person{}, want: map[string][]string{}},
+		{name: "NilInput", inputKey: "dept", inputPersons: nil, want: map[string][]string{}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+
+			gotValue := groupBy(c.inputKey, c.inputPersons)
+
+			if !reflect.DeepEqual(gotValue, c.want) {
+				t.Errorf("期待する値と違います。gotValue= %v, want= %v", gotValue, c.want)
+			}
+
+		})
+	}
+}
