cf3214ea78df594d9455938519fda9315651f902
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-06-29 00:41:54 +0900

    初回実装完了

--- go/phase1/basics/collections/06_dedupe_ordered.go
+++ go/phase1/basics/collections/06_dedupe_ordered.go
@@ -1,1 +1,18 @@
 package collections
+
+// 仕様：スライスを受け取って、その中の要素の重複を削除する関数
+// 仕様：出現順は保つ
+func deleteDuplicate(input []any) []any {
+
+	noDuplicateSlice := make([]any, 0, len(input)) // 重複していない要素を格納する
+	m := make(map[any]struct{}, len(input))        // 要素の存在を判定するmap
+
+	for _, i := range input {
+		if _, ok := m[i]; !ok { // 要素が存在しない
+			m[i] = struct{}{} // 要素の存在を格納
+			noDuplicateSlice = append(noDuplicateSlice, i)
+		}
+		// 重複している場合は何もしない
+	}
+	return noDuplicateSlice
+}
--- go/phase1/basics/collections/06_dedupe_ordered_test.go
+++ go/phase1/basics/collections/06_dedupe_ordered_test.go
@@ -1,1 +1,31 @@
 package collections
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDeleteDuplicate(t *testing.T) {
+	cases := []struct {
+		name  string
+		input []any
+		want  []any
+	}{
+		{name: "duplicateIntSlice", input: []any{3, 1, 3, 2, 4}, want: []any{3, 1, 2, 4}},
+		{name: "duplicateStringSlice", input: []any{"apple", "banana", "grape", "banana"}, want: []any{"apple", "banana", "grape"}},
+		{name: "duplicateMultiSlice", input: []any{"apple", 3, 1, "banana", "apple", 7}, want: []any{"apple", 3, 1, "banana", 7}},
+		{name: "twoDuplicateSlice", input: []any{3, 1, 2, 2, 3, 5}, want: []any{3, 1, 2, 5}},
+		{name: "noDuplicateSlice", input: []any{3, 1, 2, 5}, want: []any{3, 1, 2, 5}},
+		{name: "emptySlice", input: []any{}, want: []any{}},
+		{name: "nilSlice", input: nil, want: []any{}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			gotValue := deleteDuplicate(c.input)
+			if !reflect.DeepEqual(gotValue, c.want) {
+				t.Errorf("期待する値が違います。 gotValue= %v, want= %v", gotValue, c.want)
+			}
+		})
+	}
+}
