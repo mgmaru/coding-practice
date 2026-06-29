7febacc3de527773d262ff6ee0e0a3041438b5c8
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-06-29 16:21:45 +0900

    初回実装完了

--- go/phase1/basics/collections/07_lookup.go
+++ go/phase1/basics/collections/07_lookup.go
@@ -1,1 +1,25 @@
 package collections
+
+type User struct {
+	ID   int
+	Name string
+}
+
+// T：keyの型 K：値の型
+func findUsersByKey[T, K comparable](users []User, input []T, KeyOf func(User) T, ValueOf func(User) K) []K {
+
+	// Userのキーと値を格納するmap
+	m := make(map[T]K, len(users))
+	// 値を格納するスライス
+	result := make([]K, 0, len(users))
+
+	// キーと値を格納（ハッシュ化）
+	for _, user := range users {
+		m[KeyOf(user)] = ValueOf(user) // keyとvalueは呼び出し元で注入
+	}
+	// keyで値を引いて、スライスに格納
+	for _, v := range input { // inputはスライスから取り出しているので、順序も保証
+		result = append(result, m[v])
+	}
+	return result
+}
--- go/phase1/basics/collections/07_lookup_test.go
+++ go/phase1/basics/collections/07_lookup_test.go
@@ -1,1 +1,57 @@
 package collections
+
+import (
+	"reflect"
+	"testing"
+)
+
+// IDで引いた場合のテスト
+func TestFindUsersByKey_ID(t *testing.T) {
+	cases := []struct {
+		name    string
+		users   []User
+		input   []int
+		keyOf   func(User) int
+		valueOf func(User) string
+		want    []string
+	}{
+		{name: "Normal", users: []User{{ID: 1, Name: "A"}, {ID: 2, Name: "B"}}, input: []int{2, 1, 2}, keyOf: func(u User) int { return u.ID }, valueOf: func(u User) string { return u.Name }, want: []string{"B", "A", "B"}},
+		{name: "NameDuplicate(valueDuplicate)", users: []User{{ID: 1, Name: "A"}, {ID: 2, Name: "B"}, {ID: 3, Name: "A"}}, input: []int{2, 1, 3}, keyOf: func(u User) int { return u.ID }, valueOf: func(u User) string { return u.Name }, want: []string{"B", "A", "A"}},
+		{name: "IDDuplicate(keyDuplicate)", users: []User{{ID: 1, Name: "A"}, {ID: 2, Name: "B"}, {ID: 1, Name: "B"}}, input: []int{1, 2, 1}, keyOf: func(u User) int { return u.ID }, valueOf: func(u User) string { return u.Name }, want: []string{"B", "B", "B"}},
+		{name: "usersAndInputEmpty", users: []User{}, input: []int{}, keyOf: func(u User) int { return u.ID }, valueOf: func(u User) string { return u.Name }, want: []string{}},
+		{name: "usersEmpty", users: []User{}, input: []int{2, 1, 2}, keyOf: func(u User) int { return u.ID }, valueOf: func(u User) string { return u.Name }, want: []string{}},
+		{name: "inputEmpty", users: []User{{ID: 1, Name: "A"}, {ID: 2, Name: "B"}}, input: []int{}, keyOf: func(u User) int { return u.ID }, valueOf: func(u User) string { return u.Name }, want: []string{}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			gotValue := findUsersByKey(c.users, c.input, c.keyOf, c.valueOf)
+			if !reflect.DeepEqual(gotValue, c.want) {
+				t.Errorf("期待する値と違います。gotValue= %v, want= %v", gotValue, c.want)
+			}
+		})
+	}
+}
+
+// Nameで引いた場合のテスト
+func TestFindUsersByKey_Name(t *testing.T) {
+	cases := []struct {
+		name    string
+		users   []User
+		input   []string
+		keyOf   func(User) string
+		valueOf func(User) int
+		want    []int
+	}{
+		{name: "Normal", users: []User{{ID: 1, Name: "A"}, {ID: 2, Name: "B"}, {ID: 3, Name: "C"}}, input: []string{"C", "A", "B"}, keyOf: func(u User) string { return u.Name }, valueOf: func(u User) int { return u.ID }, want: []int{3, 1, 2}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			gotValue := findUsersByKey(c.users, c.input, c.keyOf, c.valueOf)
+			if !reflect.DeepEqual(gotValue, c.want) {
+				t.Errorf("期待する値と違います。gotValue= %v, want= %v", gotValue, c.want)
+			}
+		})
+	}
+}
