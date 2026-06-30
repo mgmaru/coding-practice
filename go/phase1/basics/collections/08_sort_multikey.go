789c7294a757535185bbc94b65ff2644218e3b51
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-07-01 00:02:06 +0900

    08_sort_multikey 初回実装

--- go/phase1/basics/collections/08_sort_multikey.go
+++ go/phase1/basics/collections/08_sort_multikey.go
@@ -1,1 +1,25 @@
 package collections
+
+import "sort"
+
+type Person struct {
+	Last string
+	Age  int
+}
+
+func sortStable(input []Person) []Person {
+
+	// inputが空もしくはnilの時
+	if len(input) == 0 {
+		return []Person{} // inputがいk空もしくはnilの場合、共通で空スライスを返す
+		// 理由：この関数を呼び出し側でnilか空スライスかを意識する必要をなくすため。
+	}
+
+	sort.Slice(input, func(i, j int) bool { // 姓を昇順でソート
+		if input[i].Last == input[j].Last { // 姓が同じだったら、年齢の降順でソート
+			return input[i].Age > input[j].Age
+		}
+		return input[i].Last < input[j].Last // それ以外は姓の昇順でソート
+	})
+	return input
+}
--- go/phase1/basics/collections/08_sort_multikey_test.go
+++ go/phase1/basics/collections/08_sort_multikey_test.go
@@ -1,1 +1,30 @@
 package collections
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSortStable(t *testing.T) {
+	cases := []struct {
+		name  string
+		input []Person
+		want  []Person
+	}{
+		{name: "Normal", input: []Person{{Last: "Sato", Age: 30}, {Last: "Ito", Age: 25}, {Last: "Suzuki", Age: 40}}, want: []Person{{Last: "Ito", Age: 25}, {Last: "Sato", Age: 30}, {Last: "Suzuki", Age: 40}}},
+		{name: "DuplicateLast", input: []Person{{Last: "Sato", Age: 30}, {Last: "Ito", Age: 25}, {Last: "Sato", Age: 40}}, want: []Person{{Last: "Ito", Age: 25}, {Last: "Sato", Age: 40}, {Last: "Sato", Age: 30}}},
+		{name: "DuplicateAge", input: []Person{{Last: "Suzuki", Age: 40}, {Last: "Ito", Age: 40}, {Last: "Sato", Age: 30}}, want: []Person{{Last: "Ito", Age: 40}, {Last: "Sato", Age: 30}, {Last: "Suzuki", Age: 40}}},
+		{name: "AllDuplicate", input: []Person{{Last: "Sato", Age: 30}, {Last: "Sato", Age: 30}, {Last: "Sato", Age: 30}}, want: []Person{{Last: "Sato", Age: 30}, {Last: "Sato", Age: 30}, {Last: "Sato", Age: 30}}},
+		{name: "NilSlice", input: nil, want: []Person{}},
+		{name: "EmptySlice", input: []Person{}, want: []Person{}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			gotValue := sortStable(c.input)
+			if !reflect.DeepEqual(gotValue, c.want) {
+				t.Errorf("期待する値と違います。gotValue= %v, want= %v", gotValue, c.want)
+			}
+		})
+	}
+}
