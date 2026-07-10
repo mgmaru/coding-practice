4086d67fafa3c0405a1c16b8f8d41256db6fab1d
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-07-10 12:47:45 +0900

    初回実装

--- go/phase1/basics/collections/10_flatten.go
+++ go/phase1/basics/collections/10_flatten.go
@@ -1,1 +1,14 @@
 package collections
+
+// var input = [][]int{{1, 2}, {3}, {4, 5, 6}} // []intにnilが入ってくる場合があることに注意
+
+func smoothTheList(input [][]int) []int {
+	oneDimList := make([]int, 0) // capはlen(input)では足りない
+
+	for _, numList := range input {
+		for _, num := range numList { // numListが空の場合、appendは実行されない
+			oneDimList = append(oneDimList, num)
+		}
+	}
+	return oneDimList
+}
--- go/phase1/basics/collections/10_flatten_test.go
+++ go/phase1/basics/collections/10_flatten_test.go
@@ -1,1 +1,31 @@
 package collections
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSmoothTheList(t *testing.T) {
+	cases := []struct {
+		name  string
+		input [][]int
+		want  []int
+	}{
+		{name: "Normal", input: [][]int{{1, 2}, {3}, {4, 5}}, want: []int{1, 2, 3, 4, 5}},
+		{name: "SingleElement", input: [][]int{{1}, {2}, {3}, {4}, {5}}, want: []int{1, 2, 3, 4, 5}},
+		{name: "ContainsEmptyElement", input: [][]int{{1, 2}, {}, {4, 5}}, want: []int{1, 2, 4, 5}},
+		{name: "AllEmptyElements", input: [][]int{{}, {}, {}}, want: []int{}},
+		{name: "ContainsNilElement", input: [][]int{{1, 2}, nil, {4, 5}}, want: []int{1, 2, 4, 5}},
+		{name: "AllNilElements", input: [][]int{nil, nil, nil}, want: []int{}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			gotvalue := smoothTheList(c.input)
+
+			if !reflect.DeepEqual(gotvalue, c.want) {
+				t.Errorf("期待する値と違います。gotValue= %v, want= %v", gotvalue, c.want)
+			}
+		})
+	}
+}
