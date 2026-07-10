cd7e7cddf4502d43374d6c5f11c8acd6191a7c97
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-07-10 13:13:43 +0900

    fix: レビューの指摘4点を修正

--- go/phase1/basics/collections/10_flatten_test.go
+++ go/phase1/basics/collections/10_flatten_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestSmoothTheList(t *testing.T) {
+func TestFlattenList(t *testing.T) {
 	cases := []struct {
 		name  string
 		input [][]int
@@ -17,11 +17,14 @@ func TestSmoothTheList(t *testing.T) {
 		{name: "AllEmptyElements", input: [][]int{{}, {}, {}}, want: []int{}},
 		{name: "ContainsNilElement", input: [][]int{{1, 2}, nil, {4, 5}}, want: []int{1, 2, 4, 5}},
 		{name: "AllNilElements", input: [][]int{nil, nil, nil}, want: []int{}},
+		// 修正：テストケース追加
+		{name: "EmptyInput", input: [][]int{}, want: []int{}},
+		{name: "NilInput", input: nil, want: []int{}},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			gotvalue := smoothTheList(c.input)
+			gotvalue := flattenList(c.input)
 
 			if !reflect.DeepEqual(gotvalue, c.want) {
 				t.Errorf("期待する値と違います。gotValue= %v, want= %v", gotvalue, c.want)
