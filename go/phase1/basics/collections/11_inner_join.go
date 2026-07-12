1534111a6d94ee309c5bc9be9beeff2c9e071f89
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-07-12 16:17:28 +0900

    feat: 11_inner_join初回実装

--- go/phase1/basics/collections/11_inner_join.go
+++ go/phase1/basics/collections/11_inner_join.go
@@ -1,1 +1,35 @@
 package collections
+
+type UserInfo struct {
+	id   int
+	name string
+}
+
+type OrderInfo struct {
+	userId int
+	item   string
+}
+
+type UserOrderInfo struct {
+	name string
+	item string
+}
+
+func innerJoinById(users []UserInfo, orders []OrderInfo) []UserOrderInfo {
+
+	userOrderInfo := make([]UserOrderInfo, 0, len(users)) // 出力スライス
+	userIdItemList := make(map[int][]string, len(orders)) // 一次マップ
+
+	// OrderInfoに関して、useridをキーとして、itemのリストを作る
+	for _, order := range orders {
+		userIdItemList[order.userId] = append(userIdItemList[order.userId], order.item)
+	}
+	// UserInfoからidをループで取り出して、キーuserIdと一致を確認
+	// 一致した場合はitemリストから１つずつ取り出して、[]UserOrderInfoにappend
+	for _, user := range users { // usersはスライスなので並びも保証
+		for _, item := range userIdItemList[user.id] { // useridのitemリストが１個以上あればappend。空の場合は何もしない。
+			userOrderInfo = append(userOrderInfo, UserOrderInfo{name: user.name, item: item})
+		}
+	}
+	return userOrderInfo
+}
--- go/phase1/basics/collections/11_inner_join_test.go
+++ go/phase1/basics/collections/11_inner_join_test.go
@@ -1,1 +1,35 @@
 package collections
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestInnerJoinById(t *testing.T) {
+	cases := []struct {
+		name   string
+		users  []UserInfo
+		orders []OrderInfo
+		want   []UserOrderInfo
+	}{
+		{name: "AllUsersIncludeOrders", users: []UserInfo{{id: 1, name: "A"}, {id: 2, name: "B"}, {id: 3, name: "C"}}, orders: []OrderInfo{{userId: 1, item: "x"}, {userId: 2, item: "y"}, {userId: 3, item: "z"}}, want: []UserOrderInfo{{name: "A", item: "x"}, {name: "B", item: "y"}, {name: "C", item: "z"}}},
+		{name: "SomeUserNotIncludeOrders（No order）", users: []UserInfo{{id: 1, name: "A"}, {id: 2, name: "B"}, {id: 3, name: "C"}}, orders: []OrderInfo{{userId: 1, item: "x"}, {userId: 3, item: "y"}}, want: []UserOrderInfo{{name: "A", item: "x"}, {name: "C", item: "y"}}},
+		{name: "SomeItemsOrderNotIncludedUser（No user）", users: []UserInfo{{id: 1, name: "A"}, {id: 2, name: "B"}, {id: 3, name: "C"}}, orders: []OrderInfo{{userId: 1, item: "x"}, {userId: 2, item: "y"}, {userId: 3, item: "z"}, {userId: 4, item: "i"}}, want: []UserOrderInfo{{name: "A", item: "x"}, {name: "B", item: "y"}, {name: "C", item: "z"}}},
+		{name: "EmptyUser", users: []UserInfo{}, orders: []OrderInfo{{userId: 1, item: "x"}, {userId: 2, item: "y"}, {userId: 3, item: "z"}}, want: []UserOrderInfo{}},
+		{name: "EmptyOrder", users: []UserInfo{{id: 1, name: "A"}, {id: 2, name: "B"}, {id: 3, name: "C"}}, orders: []OrderInfo{}, want: []UserOrderInfo{}},
+		{name: "EmptyUserAndOrder", users: []UserInfo{}, orders: []OrderInfo{}, want: []UserOrderInfo{}},
+		{name: "NilUser", users: nil, orders: []OrderInfo{{userId: 1, item: "x"}, {userId: 2, item: "y"}, {userId: 3, item: "z"}}, want: []UserOrderInfo{}},
+		{name: "NilOrder", users: []UserInfo{{id: 1, name: "A"}, {id: 2, name: "B"}, {id: 3, name: "C"}}, orders: nil, want: []UserOrderInfo{}},
+		{name: "NilUserAndOrder", users: nil, orders: nil, want: []UserOrderInfo{}},
+		{name: "OneUserPlacesMultipleOrders", users: []UserInfo{{id: 1, name: "A"}, {id: 2, name: "B"}, {id: 3, name: "C"}}, orders: []OrderInfo{{userId: 1, item: "x"}, {userId: 2, item: "y"}, {userId: 3, item: "z"}, {userId: 1, item: "i"}}, want: []UserOrderInfo{{name: "A", item: "x"}, {name: "A", item: "i"}, {name: "B", item: "y"}, {name: "C", item: "z"}}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			gotValue := innerJoinById(c.users, c.orders)
+			if !reflect.DeepEqual(gotValue, c.want) {
+				t.Errorf("期待する値と違います。gotValue= %v, want= %v", gotValue, c.want)
+			}
+		})
+	}
+}
