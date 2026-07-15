2708bcbd8fa98d031687a894d0451b50a42f9d91
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-07-15 20:52:40 +0900

    feat: 12_invert初回実装

--- go/phase1/basics/collections/12_invert.go
+++ go/phase1/basics/collections/12_invert.go
@@ -1,1 +1,19 @@
 package collections
+
+import "sort"
+
+func invertKeyValue(input map[string]int) map[int][]string {
+
+	invert_map := make(map[int][]string, len(input))
+	for key, val := range input { // mapからキーを取り出しているので、順序を保証しない。
+		invert_map[val] = append(invert_map[val], key)
+	}
+	// mapで出力するので順序は保証しない。
+	// ただし、キー自体の並びは保証しないが、値のスライスに関してはスライスなので、並び替えて順序を保証できる。
+	for _, value := range invert_map {
+		sort.SliceStable(value, func(i, j int) bool {
+			return value[i] < value[j] // 文字列の昇順でソート（大文字先小文字後（大文字の方がasciiの値が小さい） / a,b,c... / 空文字は最小）
+		})
+	}
+	return invert_map
+}
--- go/phase1/basics/collections/12_invert_test.go
+++ go/phase1/basics/collections/12_invert_test.go
@@ -1,1 +1,30 @@
 package collections
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestInvertKeyValue(t *testing.T) {
+	cases := []struct {
+		name  string
+		input map[string]int
+		want  map[int][]string
+	}{
+		{name: "NotDuplicatedValue", input: map[string]int{"a": 1, "b": 2, "c": 3}, want: map[int][]string{1: {"a"}, 2: {"b"}, 3: {"c"}}},
+		{name: "DuplicatedValue", input: map[string]int{"a": 1, "b": 2, "c": 1}, want: map[int][]string{1: {"a", "c"}, 2: {"b"}}},
+		{name: "IncludedEnptykey", input: map[string]int{"": 1, "a": 2, "b": 3}, want: map[int][]string{1: {""}, 2: {"a"}, 3: {"b"}}},
+		{name: "AllDuplicatedValues", input: map[string]int{"a": 1, "b": 1, "c": 1}, want: map[int][]string{1: {"a", "b", "c"}}},
+		{name: "IncludedUpperCaseKey", input: map[string]int{"a": 1, "b": 2, "A": 1}, want: map[int][]string{1: {"A", "a"}, 2: {"b"}}},
+		{name: "IncluededUpperCaseKeyAndEmptyKey", input: map[string]int{"a": 1, "A": 1, "b": 2, "": 1}, want: map[int][]string{1: {"", "A", "a"}, 2: {"b"}}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			gotValue := invertKeyValue(c.input)
+			if !reflect.DeepEqual(gotValue, c.want) {
+				t.Errorf("期待する値が違います。gotValue= %v, want= %v", gotValue, c.want)
+			}
+		})
+	}
+}
