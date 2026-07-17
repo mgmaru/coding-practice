e8ca0d96fcd324004a8fbea9526eca387efbdad1
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-07-17 13:02:43 +0900

    feat: 13_cumulaive初回実装

--- go/phase1/basics/collections/13_cumulative_sum.go
+++ go/phase1/basics/collections/13_cumulative_sum.go
@@ -1,1 +1,17 @@
 package collections
+
+// 狙い: 前の状態を持ち越しながら走査する。
+// 入力: [1, 2, 3, 4]
+// 出力: [1, 3, 6, 10]
+
+func calcCumlativeSum(input []int) []int {
+	output := make([]int, 0, len(input))
+	for i, num := range input {
+		if len(output) == 0 { // i == 0でも良い？
+			output = append(output, num)
+			continue
+		}
+		output = append(output, output[i-1]+num)
+	}
+	return output
+}
--- go/phase1/basics/collections/13_cumulative_sum_test.go
+++ go/phase1/basics/collections/13_cumulative_sum_test.go
@@ -1,1 +1,29 @@
 package collections
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCalcCumlativeSum(t *testing.T) {
+	cases := []struct {
+		name  string
+		input []int
+		want  []int
+	}{
+		{name: "Normal", input: []int{1, 2, 3, 4}, want: []int{1, 3, 6, 10}},
+		{name: "Normal2", input: []int{2, 4, 6, 8}, want: []int{2, 6, 12, 20}},
+		{name: "SingleInput", input: []int{1}, want: []int{1}},
+		{name: "EmptyInput", input: []int{}, want: []int{}},
+		{name: "NilInput", input: nil, want: []int{}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			gotValue := calcCumlativeSum(c.input)
+			if !reflect.DeepEqual(gotValue, c.want) {
+				t.Errorf("期待する値と違います。gotValue= %v, want= %v", gotValue, c.want)
+			}
+		})
+	}
+}
