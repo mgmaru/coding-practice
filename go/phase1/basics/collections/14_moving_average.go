9f94de13e99ca8e575860d60f4517bfdeaa85e43
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-07-19 22:34:21 +0900

    feat: 14_moving_average 初期実装

--- go/phase1/basics/collections/14_moving_average.go
+++ go/phase1/basics/collections/14_moving_average.go
@@ -1,1 +1,26 @@
 package collections
+
+func calcMovingAverage(input []int, number int) []int {
+
+	movingAverageResult := make([]int, 0, len(input))
+
+	if number > len(input) { // error
+		return movingAverageResult
+	}
+	if number <= 0 { // number >= 1
+		return movingAverageResult
+	}
+	for i, _ := range input { // iを1個ずつずらしていく
+		if i+number > len(input) { // 平均の計算対象となる数が要素を超えた場合は平均計算終わり
+			return movingAverageResult
+		}
+		// number分、inputから対象となる数を取り出して平均を計算する
+		targetNumsSum := 0
+		for j := range number { // number分要素を取り出す
+			targetNumsSum += input[i+j]
+		}
+		movingAverage := targetNumsSum / number
+		movingAverageResult = append(movingAverageResult, movingAverage)
+	}
+	return movingAverageResult
+}
--- go/phase1/basics/collections/14_moving_average_test.go
+++ go/phase1/basics/collections/14_moving_average_test.go
@@ -1,1 +1,37 @@
 package collections
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCalcMovingAverage(t *testing.T) {
+	cases := []struct {
+		name        string
+		input       []int
+		inputNumber int
+		want        []int
+	}{
+		{name: "AllPositiveIntegers", input: []int{1, 2, 3, 4, 5}, inputNumber: 3, want: []int{2, 3, 4}},
+		{name: "IncludeBothPositiveAndNegativeIntegers", input: []int{1, -2, 5, 10, -8}, inputNumber: 3, want: []int{1, 4, 2}},
+		{name: "AllPositiveIntegers", input: []int{-1, -2, -3, -4, -5}, inputNumber: 3, want: []int{-2, -3, -4}},
+		{name: "AllZeros", input: []int{0, 0, 0, 0, 0}, inputNumber: 3, want: []int{0, 0, 0}},
+		{name: "InputSizeIsSameNumberInput", input: []int{1, 2, 3, 4, 5}, inputNumber: 5, want: []int{3}},
+		{name: "EmptyInput", input: []int{}, inputNumber: 3, want: []int{}},
+		{name: "NilInput", input: nil, inputNumber: 3, want: []int{}},
+		{name: "InputNumberIsNegativeInteger", input: []int{1, 2, 3, 4, 5}, inputNumber: -1, want: []int{}},
+		{name: "InputNumberIsZero", input: []int{1, 2, 3, 4, 5}, inputNumber: 0, want: []int{}},
+		{name: "InputNumberExceedsInputSize", input: []int{1, 2, 3, 4, 5}, inputNumber: 6, want: []int{}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			gotValue := calcMovingAverage(c.input, c.inputNumber)
+
+			if !reflect.DeepEqual(gotValue, c.want) {
+				t.Errorf("期待する値とは違います。gotVlaue= %v, want= %v", gotValue, c.want)
+			}
+		})
+	}
+
+}
