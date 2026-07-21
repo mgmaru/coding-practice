e3d3117c051210bdc2219d6bba7c7743a88ad49b
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-07-22 02:33:29 +0900

    feat: 15_top_n初回実装完了

--- go/phase1/basics/collections/15_top_n.go
+++ go/phase1/basics/collections/15_top_n.go
@@ -1,1 +1,49 @@
 package collections
+
+import "sort"
+
+type Item struct {
+	Name      string
+	Frequency int
+}
+
+func displayTopN(input []Item, topN int) []Item {
+
+	validItems := make([]Item, 0, len(input))
+
+	// input and topN check
+	if topN <= 0 || len(input) == 0 {
+		return []Item{}
+	}
+
+	// frequency negative check
+	for _, e := range input {
+		if e.Frequency >= 0 {
+			validItems = append(validItems, e)
+		}
+	}
+
+	// validItems check
+	if len(validItems) == 0 {
+		return []Item{}
+	}
+
+	sort.Slice(validItems, func(i, j int) bool {
+		if validItems[i].Frequency == validItems[j].Frequency {
+			return validItems[i].Name < validItems[j].Name // もし同じ場合は名前順の昇順
+		}
+		return validItems[i].Frequency > validItems[j].Frequency
+	})
+
+	items := make([]Item, 0, topN)
+
+	for i := range topN {
+		if len(validItems) < topN { // validItemsがtopNよりも少ない場合は、validItemsを全件表示
+			items = append(items, validItems...)
+			return items
+		}
+
+		items = append(items, validItems[i])
+	}
+	return items
+}
--- go/phase1/basics/collections/15_top_n_test.go
+++ go/phase1/basics/collections/15_top_n_test.go
@@ -1,1 +1,39 @@
 package collections
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDisplayTopN(t *testing.T) {
+	cases := []struct {
+		name  string
+		input []Item
+		topN  int
+		want  []Item
+	}{
+		{name: "normal", input: []Item{{Name: "a", Frequency: 5}, {Name: "b", Frequency: 2}, {Name: "c", Frequency: 8}, {Name: "d", Frequency: 1}}, topN: 2, want: []Item{{Name: "c", Frequency: 8}, {Name: "a", Frequency: 5}}},
+		{name: "TwoItemsSameFrequency", input: []Item{{Name: "a", Frequency: 5}, {Name: "b", Frequency: 2}, {Name: "c", Frequency: 5}, {Name: "d", Frequency: 1}}, topN: 2, want: []Item{{Name: "a", Frequency: 5}, {Name: "c", Frequency: 5}}}, // 同じ頻度の場合、降順
+		{name: "TopNIsOne", input: []Item{{Name: "a", Frequency: 5}, {Name: "b", Frequency: 2}, {Name: "c", Frequency: 8}, {Name: "d", Frequency: 1}}, topN: 1, want: []Item{{Name: "c", Frequency: 8}}},
+		{name: "TopNisZero", input: []Item{{Name: "a", Frequency: 5}, {Name: "b", Frequency: 2}, {Name: "c", Frequency: 8}, {Name: "d", Frequency: 1}}, topN: 0, want: []Item{}}, // topN <=0 の時、空のスライスを返す
+		{name: "TopNIsNegativeInteger", input: []Item{{Name: "a", Frequency: 5}, {Name: "b", Frequency: 2}, {Name: "c", Frequency: 8}, {Name: "d", Frequency: 1}}, topN: 0, want: []Item{}},
+		{name: "EmptyInput", input: []Item{}, topN: 2, want: []Item{}},
+		{name: "NilInput", input: nil, topN: 2, want: []Item{}},
+		{name: "AllItemsTheSameFrequency", input: []Item{{Name: "a", Frequency: 2}, {Name: "b", Frequency: 2}, {Name: "c", Frequency: 2}, {Name: "d", Frequency: 2}}, topN: 2, want: []Item{{Name: "a", Frequency: 2}, {Name: "b", Frequency: 2}}},
+		{name: "AnElementIncludeNegativeFrequency", input: []Item{{Name: "a", Frequency: -3}, {Name: "b", Frequency: 2}, {Name: "c", Frequency: 8}, {Name: "d", Frequency: 1}}, topN: 2, want: []Item{{Name: "c", Frequency: 8}, {Name: "b", Frequency: 2}}}, // 負の頻度の要素は件数ソートから除外する。
+		{name: "ThereAreMoreNegativeFrequencyElementsThanTopN", input: []Item{{Name: "a", Frequency: -1}, {Name: "b", Frequency: 2}, {Name: "c", Frequency: -3}, {Name: "d", Frequency: -5}}, topN: 2, want: []Item{{Name: "b", Frequency: 2}}},
+		{name: "AllElementsIsNegativeFrequency", input: []Item{{Name: "a", Frequency: -1}, {Name: "b", Frequency: -2}, {Name: "c", Frequency: -3}, {Name: "d", Frequency: -5}}, topN: 2, want: []Item{}},
+		{name: "InCludeEmptyItem", input: []Item{{Name: "a", Frequency: 5}, {Name: "b", Frequency: 2}, {Name: "c", Frequency: 8}, {Name: "", Frequency: 7}}, topN: 2, want: []Item{{Name: "c", Frequency: 8}, {Name: "", Frequency: 7}}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			gotValue := displayTopN(c.input, c.topN)
+
+			if !reflect.DeepEqual(gotValue, c.want) {
+				t.Errorf("期待する値ではありません。 gotValue=%v, want= %v", gotValue, c.want)
+			}
+		})
+	}
+
+}
