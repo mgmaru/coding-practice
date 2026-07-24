4a2c72c6cd176ff17ea5ab8f6c35ab3c23238124
kajiwarahiroaki <megamarumax6188@gmail.com>
2026-07-24 15:59:05 +0900

    fix: TestGetTargetIndexBinarySearchを追加, ソートされていないテストケースを削除

--- go/phase1/basics/algo/01_binary_search_test.go
+++ go/phase1/basics/algo/01_binary_search_test.go
@@ -14,7 +14,7 @@ import (
 //   - 1要素: a=[5],target=5 → 0 / target=3 → -1
 // TODO: テストを書く
 
-func TestGetTargetIndex(t *testing.T) {
+func TestGetTargetIndexOriginal(t *testing.T) {
 	cases := []struct {
 		name   string
 		input  []int
@@ -33,12 +33,44 @@ func TestGetTargetIndex(t *testing.T) {
 		{name: "InputContainsDuplicateElementsAndTargetduplicated", input: []int{1, 3, 7, 7, 9, 11}, target: 7, want: 2}, // ターゲットが重複した場合、インデックスが小さいものを返す
 		{name: "AllInputSameElementAndIncludedTarget", input: []int{3, 3, 3, 3, 3, 3}, target: 3, want: 0},
 		{name: "AllInputSameElementAndNotIncludedTarget", input: []int{3, 3, 3, 3, 3, 3}, target: 1, want: -1}, // 不要なような気がするが、Includedを確かめたら、Not Includedも確かめるべきかと思った
-		{name: "InputContainsNegativeInteger", input: []int{1, -3, 5, -7, 9, -11}, target: -7, want: 3},
+		// {name: "InputContainsNegativeInteger", input: []int{1, -3, 5, -7, 9, -11}, target: -7, want: 3},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			gotValue := getTargetIndex(c.input, c.target)
+			gotValue := getTargetIndexOriginal(c.input, c.target)
+			if !reflect.DeepEqual(gotValue, c.want) {
+				t.Errorf("期待する値と違います。gotValue= %v, want= %v", gotValue, c.want)
+			}
+		})
+	}
+}
+
+func TestGetTargetIndexBinarySearch(t *testing.T) {
+	cases := []struct {
+		name   string
+		input  []int
+		target int
+		want   int
+	}{
+		{name: "TargetIncludedInput", input: []int{1, 3, 5, 7, 9, 11}, target: 7, want: 3},
+		{name: "TargetNotIncludedInput", input: []int{1, 3, 5, 7, 9, 11}, target: 4, want: -1},
+		{name: "TargetBeginningOfInput", input: []int{1, 3, 5, 7, 9, 11}, target: 1, want: 0},
+		{name: "TargetEndgOfInput", input: []int{1, 3, 5, 7, 9, 11}, target: 11, want: 5},
+		{name: "EmptyInput", input: []int{}, target: 7, want: -1},
+		{name: "NilInput", input: nil, target: 7, want: -1},
+		{name: "InputIsOneElementAndTargetIncludedInput", input: []int{7}, target: 7, want: 0},
+		{name: "InputIsOneElementAndTargetNotIncludedInput", input: []int{7}, target: 3, want: -1},
+		{name: "InputContainsDuplicateElementsAndTargetNotduplicated", input: []int{1, 3, 7, 7, 9, 11}, target: 3, want: 1},
+		{name: "InputContainsDuplicateElementsAndTargetduplicated", input: []int{1, 3, 7, 7, 9, 11}, target: 7, want: 2}, // ターゲットが重複した場合、インデックスが小さいものを返す
+		{name: "AllInputSameElementAndIncludedTarget", input: []int{3, 3, 3, 3, 3, 3}, target: 3, want: 0},
+		{name: "AllInputSameElementAndNotIncludedTarget", input: []int{3, 3, 3, 3, 3, 3}, target: 1, want: -1}, // 不要なような気がするが、Includedを確かめたら、Not Includedも確かめるべきかと思った
+		// {name: "InputContainsNegativeInteger", input: []int{1, -3, 5, -7, 9, -11}, target: -7, want: 3}, // 修正：ソートできていないのでそもそも二分探索の利用条件に合っていないので削除
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			gotValue := getTargetIndexBinarySearch(c.input, c.target)
 			if !reflect.DeepEqual(gotValue, c.want) {
 				t.Errorf("期待する値と違います。gotValue= %v, want= %v", gotValue, c.want)
 			}
